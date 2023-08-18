6da1b3e9160db95393323adf7b7d040ebd412f54
Ri-2020 <rohitgupta111abcd@gmail.com>
2023-08-18 13:57:59 +0530

    Some minor changes

--- src/index.js
+++ src/index.js
@@ -2,17 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { BrowserRouter, Routes, Route, HashRouter } from "react-router-dom";
+import { Routes, Route, HashRouter } from "react-router-dom";
 import Terms from './terms';
 import Privacy from './privacy';
-// "homepage": "https://getaloy.github.io", -> for package.json 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     {/* <App /> */}
     <HashRouter>
       <Routes>
-        {/* <Route path='/' element={<App/>}> */}
           <Route index element={<App />} />
           <Route path="" element={<App />} />
           <Route path="privacy" element={<Privacy />} />
