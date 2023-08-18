636d66c78fef7349c1ed15baf965fa61fa89729d
Ri-2020 <rohitgupta111abcd@gmail.com>
2023-08-18 19:17:20 +0530

    changes in indexjs hashrouter -> browserrouter

--- src/index.js
+++ src/index.js
@@ -2,21 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { Routes, Route, HashRouter } from "react-router-dom";
+import { BrowserRouter, Routes, Route, HashRouter } from "react-router-dom";
 import Terms from './terms';
 import Privacy from './privacy';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     {/* <App /> */}
-    <HashRouter>
+    <BrowserRouter>
       <Routes>
           <Route index element={<App />} />
           <Route path="" element={<App />} />
           <Route path="privacy" element={<Privacy />} />
           <Route path="terms" element={<Terms />} />
       </Routes>
-    </HashRouter>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
