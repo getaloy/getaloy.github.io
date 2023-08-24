fb5054ad304d395b25b1df685523d2648dbba27e
Ri-2020 <rohitgupta111abcd@gmail.com>
2023-08-24 11:54:11 +0530

    routing changed to hashrouter

--- src/index.js
+++ src/index.js
@@ -2,20 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { Routes, Route, BrowserRouter } from "react-router-dom";
+import { Routes, Route, HashRouter } from "react-router-dom";
 import Terms from './terms';
 import Privacy from './privacy';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
           <Route index element={<App />} />
           <Route path="" element={<App />} />
           <Route path="privacy" element={<Privacy />} />
           <Route path="terms" element={<Terms />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
 
